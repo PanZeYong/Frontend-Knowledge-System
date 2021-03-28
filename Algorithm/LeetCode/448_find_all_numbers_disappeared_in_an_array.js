ac079a865b830f5ae5faa88b92a080a3ac615f86
PANJU <04120513panzeyong@gmail.com>
2021-03-28 16:32:45 +0800

    leetcode: 找到所有数组中消失的数字

---
+++ Algorithm/LeetCode/448_find_all_numbers_disappeared_in_an_array.js
@@ -0,0 +1,96 @@
+/**
+ * 题目：找到所有数组中消失的数字
+ * 
+ * 问题描述：
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ * 
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ * 
+ * 
+ * 示例:
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ * 
+ * 输出:
+ * [5,6]
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
+var findDisappearedNumbers = function(nums) {
+    const length = nums.length
+    const result = []
+
+    for (let i = 1; i <= length; i++) {
+        if (nums.indexOf(i) === -1) {
+            result.push(i)
+        }
+    }
+
+    return result
+};
+
+/**
+ * 原地修改数组
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
+ var findDisappearedNumbers = function(nums) {
+    const length = nums.length
+    const result = []
+
+    for (let i = 0; i < length; i++) {
+        const index = Math.abs(nums[i]) - 1
+        if (nums[index] > 0) {
+            nums[index] *= -1
+        }
+    }
+
+    for (let i = 0; i < length; i++) {
+        if (nums[i] > 0) {
+            result.push(i + 1)
+        }
+    }
+
+    return result
+};
+
+/**
+ * 原地修改数组
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
+ var findDisappearedNumbers = function(nums) {
+    const length = nums.length
+    const result = []
+
+    for (let i = 0; i < length; i++) {
+        const index = (nums[i] - 1) % length
+        nums[index] += length
+    }
+
+    for (let i = 0; i < length; i++) {
+        if (nums[i] <= length) {
+            result.push(i + 1)
+        } 
+    }
+
+    return result
+};
